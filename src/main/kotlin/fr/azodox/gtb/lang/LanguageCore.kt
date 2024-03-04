6238cedeebf4c922730b7ceb90059ed7b1d1f029
Azodox <likemoi99@gmail.com>
2024-03-04 05:24:23 +0100

    fix: language core working properly

--- src/main/kotlin/fr/azodox/gtb/lang/LanguageCore.kt
+++ src/main/kotlin/fr/azodox/gtb/lang/LanguageCore.kt
@@ -3,8 +3,11 @@ package fr.azodox.gtb.lang
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.decodeFromStream
+import java.nio.file.FileSystems
 import java.nio.file.Files
 import java.nio.file.Paths
+import java.util.zip.ZipEntry
+import kotlin.io.path.inputStream
 import kotlin.io.path.isDirectory
 
 class LanguageCore {
@@ -15,12 +18,13 @@ class LanguageCore {
 
     @OptIn(ExperimentalSerializationApi::class)
     fun init(){
-        val url = this::class.java.getResource("locales")
+        val url = this::class.java.getResource("/locales")
         if (url != null) {
-            val path = Paths.get(url.toURI())
+            val fs = FileSystems.newFileSystem(url.toURI(), mapOf("create" to "true"))
+            val path = fs.getPath("/locales")
             Files.walk(path).forEach {
-                if(it.isDirectory() && it.toFile().listFiles() != null) {
-                    it.toFile().listFiles()!!.forEach {file ->
+                if(it.isDirectory()) {
+                    Files.list(path).forEach {file ->
                         val language = Json.decodeFromStream<Language>(file.inputStream())
                         languages[language.locale] = language
                         println("Loaded language : ${language.name} (${language.locale})")
