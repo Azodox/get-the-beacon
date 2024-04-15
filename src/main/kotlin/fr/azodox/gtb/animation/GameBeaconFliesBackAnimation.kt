0c5e2c7ea39ab00d182bbeddade9c849f3b9802e
Azodox <likemoi99@gmail.com>
2024-04-16 01:13:03 +0200

    feat(animation): finished beacon fly back animation

--- src/main/kotlin/fr/azodox/gtb/animation/GameBeaconFliesBackAnimation.kt
+++ src/main/kotlin/fr/azodox/gtb/animation/GameBeaconFliesBackAnimation.kt
@@ -4,16 +4,36 @@ import fr.azodox.gtb.game.GAME_BEACON_PICKED_UP_CACHE_PREFIX_CONSTANT
 import fr.azodox.gtb.game.GameBeacon
 import fr.azodox.gtb.util.CacheHelper
 import org.bukkit.Location
+import org.bukkit.Particle
 import org.bukkit.entity.BlockDisplay
 import org.bukkit.scheduler.BukkitRunnable
 
 class GameBeaconFliesBackAnimation(private val beacon: GameBeacon, private val targetLocation: Location) : BukkitRunnable() {
 
-    private var timer = 0
+    private val display = CacheHelper.get<BlockDisplay>(GAME_BEACON_PICKED_UP_CACHE_PREFIX_CONSTANT + "_display")
 
     override fun run() {
-        val display = CacheHelper.get<BlockDisplay>(GAME_BEACON_PICKED_UP_CACHE_PREFIX_CONSTANT + "_display") ?: return
-        display.transformation
-        timer++
+        if (display == null){
+            cancel()
+            return
+        }
+
+        val location = display.location
+        if (targetLocation.getNearbyEntitiesByType(BlockDisplay::class.java, 0.5, 1.0).contains(display)){
+            beacon.spawnAtDefaultLocation()
+            display.remove()
+            CacheHelper.remove(GAME_BEACON_PICKED_UP_CACHE_PREFIX_CONSTANT + "_display")
+            CacheHelper.remove(GAME_BEACON_PICKED_UP_CACHE_PREFIX_CONSTANT + "_holder")
+            cancel()
+            return
+        }
+
+        val vector = targetLocation.toVector().subtract(location.toVector()).normalize()
+        location.add(vector)
+
+        location.add(0.0, 0.5, 0.0)
+
+        display.teleport(location)
+        location.world.spawnParticle(Particle.CLOUD, location, 20, 0.0, 0.0, 0.0, 0.0)
     }
 }
